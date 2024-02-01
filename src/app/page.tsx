80db4517d6f0414051ca1c4966d1f8c72c4aa565
trauty <trautwein.maximilian04@gmail.com>
2024-02-01 22:14:41 +0100

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen">
         <div>
-          <Image 
-            src="/electos_logo.svg"
-            width="536"
-            height="131"
-            alt="Logo"
-          >
-
-          </Image>
+          
         </div>
     </main>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Logo } from ".";
+
+
+export const NavBar = () => {
+    return (
+        <div className="fixed w-[100%] bg-electos-black-950">
+            <div className="flex w-[80%] justify-between m-auto py-5">
+                <div>
+                    <Link href="">
+                        <Logo width={170} height={36}/>
+                    </Link>
+                </div>
+                <div>
+                    <nav className="h-full">
+                        <ul className="h-full flex gap-6 justify-center items-center text-electos-white">
+                            <li>
+                                <Link href="">Passive Komp.</Link>
+                            </li>
+                            <li>
+                                <Link href="">Aktive Komp.</Link>
+                            </li>
+                            <li>
+                                <Link href="">Mikroprozessoren</Link>
+                            </li>
+                            <div className="ml-8 flex gap-4">
+                                <li>
+                                    <Link href="">Anmelden</Link>
+                                </li>
+                                <li>
+                                    <Link href="">Registrieren</Link>
+                                </li>
+                            </div>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    )
+}
