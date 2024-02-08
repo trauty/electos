36a6f254384756206d26bc65bfe33a1c673ea527
trauty <trautwein.maximilian04@gmail.com>
2024-02-08 12:58:33 +0100

    backup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen w-screen flex items-center justify-center">
         <div>
-          
+          LEL
         </div>
     </main>
   );
